725fcfd51cbebb55ba635e6bd0b536ae53c65463
sgb <supgb96@gmail.com>
2018-08-31 18:32:34 +0800

    fix bugs of insert method

--- ExtendedChain/extendedChain.hpp
+++ ExtendedChain/extendedChain.hpp
@@ -30,4 +30,112 @@ class extendedChain :  public extendedLinearList<T>,
         virtual void push_back(const T&);
 };
 
+template <typename T>
+extendedChain<T>::extendedChain() :
+Chain<T>::Chain()
+{        
+    lastNode = nullptr;
+}
+
+template <typename T>
+extendedChain<T>::extendedChain(const extendedChain<T>& chain) {
+    this->listSize = chain.listSize;
+    chainNode<T>* t = chain.firstNode;
+    if (t == nullptr) {
+        this->firstNode = nullptr;
+        return;
+    }
+    
+    this->firstNode = new chainNode<T>(t->element);
+    chainNode<T>* s = this->firstNode;
+    while(t->next != nullptr) {
+        t = t->next;
+        s->next = new chainNode<T>(t->element);
+        s = s->next;
+        lastNode = s;
+    }
+}
+
+template <typename T>
+extendedChain<T>::~extendedChain() {
+    lastNode = nullptr;
+}
+
+template <typename T>
+bool extendedChain<T>::empty() const {
+    return this->firstNode == nullptr;
+}
+
+template <typename T>
+int extendedChain<T>::size() const {
+    return this->listSize;
+}
+
+template <typename T>
+T& extendedChain<T>::get(int theIndex) const {
+    return Chain<T>::get(theIndex);
+}
+
+template <typename T>
+int extendedChain<T>::indexOf(const T& theElement) const {
+    return Chain<T>::indexOf(theElement);
+}
+
+template <typename T>
+void extendedChain<T>::erase(int theIndex) {
+    this->checkIndex(theIndex);
+    chainNode<T>* deleteNode;
+    if (theIndex == this->listSize-1) {
+        /* 要删除的元素位于链表的最后一位 */
+        deleteNode = lastNode;
+        chainNode<T>* p = this->firstNode;
+        for(size_t i = 0; i < theIndex-1; i++)
+        {
+            p = p->next;
+        }
+        lastNode = p;
+        delete deleteNode;
+        this->listSize--;
+    } else {
+        Chain<T>::erase(theIndex);
+    }
+}
+
+template <typename T>
+void extendedChain<T>::insert(int theIndex, const T& theElement) {
+    
+    if (theIndex < 0 || theIndex > this->listSize) {
+        throw "out of range -- when insert element [extendedChain]";
+    }
+    if (theIndex == this->listSize && theIndex != 0) {
+        /* 在链表尾部插入元素 */
+        chainNode<T>* p = lastNode;
+        lastNode = new chainNode<T>(theElement, lastNode->next);
+        p->next = lastNode;
+        this->listSize++;
+    } else {
+        Chain<T>::insert(theIndex, theElement);
+    }
+    if (lastNode == nullptr) {
+        lastNode = this->firstNode;
+    }
+    
+}
+
+template <typename T>
+void extendedChain<T>::output(std::ostream& out) const {
+    Chain<T>::output(out);
+}
+
+template <typename T>
+void extendedChain<T>::clear() {
+    Chain<T>::~Chain();
+    lastNode = nullptr;
+}
+
+template <typename T>
+void extendedChain<T>::push_back(const T& theElement) {
+    insert(this->listSize-1, theElement);
+}
+
 #endif // __EXTENDEDCHAIN_H
