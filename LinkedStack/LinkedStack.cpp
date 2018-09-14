4a18102ca3b700724852a9f3f774c709e93e4b4f
sgb <supgb96@gmail.com>
2018-09-15 00:04:04 +0800

    Stack with linked description

---
+++ LinkedStack/LinkedStack.cpp
@@ -0,0 +1,56 @@
+#include "LinkedStack.hpp"
+
+template <class T>
+void testOnStack(LinkedStack<T>& Stack, int* elements, size_t size, char flag = 'p');   // Note: The param with a default value must be at the end of params list.
+
+int main(int argc, char const *argv[]) {
+    try {
+        LinkedStack<int> stack;    
+        int elements[] = {21, 31, 41, 51, 61, 71};
+        testOnStack(stack, elements, sizeof(elements)/sizeof(int));
+        std::cout << "\n^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n" << std::endl;
+        testOnStack(stack, elements, sizeof(elements)/sizeof(int), 'd');
+        std::cout << "\nEND OF TEST......\n" << std::endl;
+    } catch(char const *e) {
+        std::cerr << e << std::endl;
+    }
+    return 0;
+}
+
+template <class T>
+void testOnStack(LinkedStack<T>& Stack, int* elements, size_t size, char flag) {
+    if(flag == 'p') {
+        std::cout << "\n-----Start Test-----\n" << std::endl;
+        std::cout << "\n-----Test before any pushing-----\n" << std::endl;
+        std::cout << "[Test method size]: " << Stack.size() << std::endl;
+        std::cout << "[Test method empty]: " << "Is Stack empty? ans = "
+                << (Stack.empty()?"Yes!":"No!") << std::endl;
+
+        // Push elements
+        std::cout << "Test status during pushing" << std::endl;
+        for(size_t i = 0; i < size; ++i) {
+            Stack.push(elements[i]);
+            std::cout << "\n-----Test after push " << elements[i] << "-----\n" << std::endl;
+            std::cout << "[Test method empty]: " << "Is Stack empty? ans = "
+                    << (Stack.empty()?"Yes!":"No!") << std::endl;
+            std::cout << "[Test method size]: " << Stack.size() << std::endl;
+            std::cout << "[Test operator<< override]: " << "Stack contains:"
+                    << Stack << std::endl;
+            std::cout << "[Test method top]: " << "The top of Stack is " << Stack.top() << std::endl;
+        }
+    } else if(flag = 'd') {
+        std::cout << "\n*****Test status during popping*****\n" << std::endl;
+        for(size_t i = 0; i < size; ++i) {
+            std::cout << "\n-----Test after popping " << Stack.top() << "-----\n" << std::endl;
+            Stack.pop();
+            std::cout << "[Test method empty]: " << "Is Stack empty? ans = "
+                    << (Stack.empty()?"Yes!":"No!") << std::endl;
+            std::cout << "[Test method size]: " << Stack.size() << std::endl;
+            std::cout << "[Test operator<< override]: " << "Stack contains:"
+                    << Stack << std::endl;
+            // std::cout << "[Test method top]: " << "The top of Stack is " << Stack.top() << std::endl;
+        }
+    } else {
+        std::cout << "Wrong option!" << std::endl;
+    }
+}
---
+++ LinkedStack/LinkedStack.hpp
@@ -0,0 +1,87 @@
+#ifndef __LINKEDSTACK_H
+#define __LINKEDSTACK_H
+
+#include <iostream>
+#include "Chain.hpp"
+#include "Stack.hpp"
+
+template <class T>
+class LinkedStack : public Stack<T>{
+    template <class Y>
+    friend std::ostream& operator<<(std::ostream&, const LinkedStack<Y>&);
+private:
+    chainNode<T> *stackTop;
+    size_t stackSize;
+public:
+    LinkedStack();
+    LinkedStack(const LinkedStack<T>&);
+
+    ~LinkedStack();
+    bool empty() const {return stackSize == 0;}
+    size_t size() const {return stackSize;}
+    T& top() const;
+    void pop();
+    void push(const T&);
+};
+
+template <class T>
+LinkedStack<T>::LinkedStack():
+stackTop(nullptr),
+stackSize(0) 
+{}
+
+template <class T>
+LinkedStack<T>::LinkedStack(const LinkedStack<T>& Stack) {
+    chainNode<T> *p = this->stackTop,
+                 *t = Stack.stackTop;
+    for(size_t i = 0; i < Stack.stackSize; ++i, p=p->next, t=t->next) {
+        p->element = t->element;
+    }
+}
+
+template <class T>
+LinkedStack<T>::~LinkedStack() {
+    chainNode<T> *p = stackTop;
+    for(size_t i = 0; i < stackSize; ++i) {
+        p = p->next;
+        std::cout << "[~LinkedStack] Destroy " << stackTop->element << std::endl;
+        delete stackTop;
+        stackTop = p;
+    }
+}
+
+template <class T>
+T& LinkedStack<T>::top() const {
+    if(stackTop == nullptr) {
+        throw "The Stack is empty!";
+    }
+    return stackTop->element;
+}
+
+template <class T>
+void LinkedStack<T>::push(const T& element) {
+    stackTop = new chainNode<T>(element, stackTop);
+    stackSize++;
+}
+
+template <class T>
+void LinkedStack<T>::pop() {
+    if(stackTop == nullptr) {
+        throw "The Stack is empty!";
+    }
+    chainNode<T> *deleteNode = stackTop;
+    stackTop = stackTop->next;
+    delete deleteNode;
+    stackSize--;
+}
+
+template <class T>
+std::ostream& operator<<(std::ostream& out, const LinkedStack<T>& Stack) {
+    chainNode<T> *p = Stack.stackTop;
+    for(size_t i = 0; i < Stack.stackSize; ++i, p=p->next) {
+        out << p->element << " ";
+    }
+    return out;
+}
+
+#endif // __LINKEDSTACK_H
